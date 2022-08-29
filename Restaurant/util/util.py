7eef8634f4e6d09703cbd57c334858173fc2cb64
shaikshan <shaikroshan1997@gmail.com>
2022-08-29 20:57:08 +0530

    Updated

--- Restaurant/util/util.py
+++ Restaurant/util/util.py
@@ -4,6 +4,8 @@ import os,sys
 from Restaurant.logger import logging
 from Restaurant.exception import RestaurantException
 import pandas as pd
+import dill
+
 def read_yaml_file(file_path:str):
     """
     Reads a yaml file and returns the content as a dictionary.
@@ -29,12 +31,67 @@ def replace(value):
     except Exception as e:
         raise RestaurantException(e,sys) from e
 
-def delete_columns(columns:list,df:pd.DataFrame):
+def write_yaml_file(file_path:str,data:dict=None):
+    """
+    Create yaml file 
+    file_path: str
+    data: dict
+    """
     try:
-        for column in df.columns:
-            if column in columns:
-                df.drop(columns=column,axis=1,inplace=True)
-        return df
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path,"w") as yaml_file:
+            if data is not None:
+                yaml.dump(data,yaml_file)
+    except Exception as e:
+        raise RestaurantException(e,sys)
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise RestaurantException(e, sys) from e
+
+def load_numpy_array_data(file_path: str) -> np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise RestaurantException(e, sys) from e
+
+
+def save_object(file_path:str,obj):
+    """
+    file_path: str
+    obj: Any sort of object
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+    except Exception as e:
+        raise RestaurantException(e,sys) from e
+
+def load_object(file_path:str):
+    """
+    file_path: str
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise RestaurantException(e,sys) from e
 
