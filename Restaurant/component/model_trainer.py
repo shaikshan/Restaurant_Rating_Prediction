a514209f2681af0a568fa986d156ae76e8bc3da5
shaikshan <shaikroshan1997@gmail.com>
2022-09-02 15:18:30 +0530

    Updated

--- Restaurant/component/model_trainer.py
+++ Restaurant/component/model_trainer.py
@@ -3,7 +3,31 @@ from Restaurant.logger import logging
 from Restaurant.entity.artifact_entity import DataTransformationArtifact,ModelTrainerArtifact
 from Restaurant.entity.config_entity import ModelTrainerConfig
 import os,sys
+from Restaurant.util.util import *
+class RestaurantEstimatorModel:
+    def __init__(self, preprocessing_object, trained_model_object):
+        """
+        TrainedModel constructor
+        preprocessing_object: preprocessing_object
+        trained_model_object: trained_model_object
+        """
+        self.preprocessing_object = preprocessing_object
+        self.trained_model_object = trained_model_object
 
+    def predict(self, X):
+        """
+        function accepts raw inputs and then transformed raw input using preprocessing_object
+        which gurantees that the inputs are in the same format as the training data
+        At last it perform prediction on transformed features
+        """
+        transformed_feature = self.preprocessing_object.transform(X)
+        return self.trained_model_object.predict(transformed_feature)
+
+    def __repr__(self):
+        return f"{type(self.trained_model_object).__name__}()"
+
+    def __str__(self):
+        return f"{type(self.trained_model_object).__name__}()"
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,
@@ -16,7 +40,23 @@ class ModelTrainer:
             raise RestaurantException(e,sys) from e
     def initiate_model_trainer(self)->ModelTrainerArtifact:
         try:
-            pass
+            logging.info(f"Loading transformed training dataset")
+            transformed_train_file_path = self.data_transformation_artifact.transformed_train_file_path
+            train_array = load_numpy_array_data(file_path=transformed_train_file_path)
+
+            logging.info(f"Loading transformed testing data")
+            transformed_test_file_path = self.data_transformation_artifact.transformed_test_file_path
+            test_array = load_numpy_array_data(file_path=transformed_test_file_path)
+
+            logging.info(f"Splitting training and testing input and target features")
+            x_train,y_train,x_test,y_test = train_array[:,:-1],train_array[:,-1],test_array[:,:-1],test_array[:,-1]
+
+            logging.info(f"Extracting model config file path")
+            model_config_file_path = self.model_trainer_config.model_config_file_path
+
+            logging.info(f"Initializing Model Factory class using above model config file:{model_config_file_path}")
+
+            
         except Exception as e:
             raise RestaurantException(e,sys) from e
     def __del__(self):
