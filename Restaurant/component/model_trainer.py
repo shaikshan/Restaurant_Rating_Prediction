84f7dd241faf1ed5833eb75ef8fb25f57d20d6cf
shaikshan <shaikroshan1997@gmail.com>
2022-09-02 21:47:22 +0530

    Updated

--- Restaurant/component/model_trainer.py
+++ Restaurant/component/model_trainer.py
@@ -1,9 +1,12 @@
+from Restaurant.entity.model_factory import GridSearchBestModel, MetricInfoArtifact, ModelFactory, evaluate_classification_model
 from Restaurant.exception import RestaurantException
 from Restaurant.logger import logging
 from Restaurant.entity.artifact_entity import DataTransformationArtifact,ModelTrainerArtifact
 from Restaurant.entity.config_entity import ModelTrainerConfig
 import os,sys
 from Restaurant.util.util import *
+from typing import List
+
 class RestaurantEstimatorModel:
     def __init__(self, preprocessing_object, trained_model_object):
         """
@@ -54,13 +57,55 @@ class ModelTrainer:
             logging.info(f"Extracting model config file path")
             model_config_file_path = self.model_trainer_config.model_config_file_path
 
-            logging.info(f"Initializing Model Factory class using above model config file:{model_config_file_path}")
+            logging.info(f"Initializing model Factory class using above model config file:{model_config_file_path}")
+            model_factory = ModelFactory(model_config_path=model_config_file_path)
 
+            base_accuracy = self.model_trainer_config.base_accuracy
+            logging.info(f"Expected accuracy:{base_accuracy}")
+
+            logging.info(f"Initiating operation model selection")
+            best_model = model_factory.get_best_model(X=x_train,y=y_train,base_accuracy=base_accuracy)
+
+            logging.info(f"Best model found on training dataset:{best_model}")
+
+            logging.info(f"Extracting trained model list:")
+            grid_searched_best_model_list:List[GridSearchBestModel] = model_factory.grid_searched_best_model_list
             
+            model_list = [model.best_model for model in grid_searched_best_model_list]
+            logging.info(f"Evaluation all trained model on training and testing dataset both")
+            metric_info:MetricInfoArtifact = evaluate_classification_model(model_list=model_list,
+                                                                            X_train=x_train,
+                                                                            y_train=y_train,
+                                                                            X_test=x_test,
+                                                                            y_test=y_test,
+                                                                            base_accuracy=base_accuracy)
+
+            logging.info(f"Best model found on both training and testing dataset")
+
+            preprocessing_obj = load_object(file_path=self.data_transformation_artifact.preprocessed_object_file_path)
+            model_object = metric_info.model_object
+
+            trained_model_file_path = self.model_trainer_config.trained_model_file_path
+            restaurant_model = RestaurantEstimatorModel(preprocessing_object=preprocessing_obj,
+                                                            trained_model_object=model_object)
+            logging.info(f"Saving model at path:{trained_model_file_path}")
+            save_object(file_path=trained_model_file_path,obj=restaurant_model)
+
+
+            model_trainer_artifact = ModelTrainerArtifact(is_trained=True,
+                                            message="Model Trained Succesfully",
+                                            trained_model_file_path=trained_model_file_path,
+                                            train_rmse=metric_info.train_rmse,
+                                            test_rmse=metric_info.test_rmse,
+                                            train_accuracy=metric_info.train_accuracy,
+                                            test_accuracy=metric_info.test_accuracy,
+                                            model_accuracy=metric_info.model_accuracy)
+
+            logging.info(f"Model Trainer Artifact:{model_trainer_artifact}")
+            return model_trainer_artifact
+
         except Exception as e:
             raise RestaurantException(e,sys) from e
     def __del__(self):
-        try:
-            logging.info(f"{'>>'*30}Model_Training_End{'<<'*30}")
-        except Exception as e:
-            raise RestaurantException(e,sys) from e
+        logging.info(f"{'>>'*30}Model_Training_End{'<<'*30}")
+  
