2ef21dee532510c1378030eabf192fb489680221
shaikshan <shaikroshan1997@gmail.com>
2022-09-09 18:10:31 +0530

    Updated

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+from crypt import methods
+from email import message
+from email.policy import default
+from genericpath import isfile
 from flask import Flask,request
 import sys
+
+from flask.config import Config
 from Restaurant.util.util import read_yaml_file,write_yaml_file
 from Restaurant.logger import logging
 from Restaurant.exception import RestaurantException
@@ -26,6 +32,60 @@ RESTAURANT_DATA_KEY = "restaurant_data"
 RATE_VALUE_KEY = "Rate"
 
 app = Flask(__name__)
+
+@app.route('/artifact',defaults={'req_path':'Restaurant'})
+@app.route('/artifact/<path:req_path>')
+def render_artifact_dir(req_path):
+    os.makedirs('Restaurant', exist_ok=True)
+    #Joining the base and the requested path
+    print(f"req_path:{req_path}")
+    abs_path = os.path.join(req_path)
+    print(abs_path)
+    #Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    #Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        if ".html" in abs_path:
+            with open(abs_path,"r", encoding="utf-8") as file:
+                content = ''
+                for line in file.readlines():
+                    content = f"{content}{line}"
+                return content
+        return send_file(abs_path)
+
+    #Show directory contents
+    files = {os.path.join(abs_path,file_name): file_name for file_name in os.listdir(abs_path) if
+                "artifact" in os.path.join(abs_path,file_name)}
+
+    result = {
+        "files":files,
+        "parent_folder": os.path.dirname(abs_path),
+        "parent_label": abs_path
+    }
+    return render_template("files.html",result=result)
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        return str(e)
+
+@app.route('/train',methods=['GET','POST'])
+def train():
+    message= ""
+    pipeline = Pipeline(config=Configuration(current_time_stamp=get_current_time_stamp()))
+    if not Pipeline.experiment.running_status:
+        message = "Training started."
+        pipeline.start()
+    else:
+        message = "Training is already in progress"
+    context = {
+        "experiment":pipeline.get_experiment_status().to_html(classes='table table-striped col-12'),
+        "message":message}
+    return render_template('train.html',context=context)
 
 
 @app.route('/predict',methods= ['GET','POST'])
@@ -57,7 +117,80 @@ def predict():
             RATE_VALUE_KEY:Rate_value,
         }
         return render_template('predict.html',context=context)
-    return render_template('predict.htm',context=context)
+    return render_template('predict.html',context=context)
+
+@app.route('/saved_models', defaults={'req_path': 'saved_models'})
+@app.route('/saved_models/<path:req_path>')
+def saved_models_dir(req_path):
+    os.makedirs("saved_models", exist_ok=True)
+    # Joining the base and the requested path
+    print(f"req_path: {req_path}")
+    abs_path = os.path.join(req_path)
+    print(abs_path)
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        return send_file(abs_path)
+
+    # Show directory contents
+    files = {os.path.join(abs_path, file): file for file in os.listdir(abs_path)}
+
+    result = {
+        "files": files,
+        "parent_folder": os.path.dirname(abs_path),
+        "parent_label": abs_path
+    }
+    return render_template('saved_models_files.html', result=result)
+
+@app.route("/update_model_config", methods=['GET', 'POST'])
+def update_model_config():
+    try:
+        if request.method == 'POST':
+            model_config = request.form['new_model_config']
+            model_config = model_config.replace("'", '"')
+            print(model_config)
+            model_config = json.loads(model_config)
+
+            write_yaml_file(file_path=MODEL_CONFIG_FILE_PATH, data=model_config)
+
+        model_config = read_yaml_file(file_path=MODEL_CONFIG_FILE_PATH)
+        return render_template('update_model.html', result={"model_config": model_config})
+
+    except  Exception as e:
+        logging.exception(e)
+        return str(e)
+
+
+@app.route(f'/logs', defaults={'req_path': f'{LOG_FOLDER_NAME}'})
+@app.route(f'/{LOG_FOLDER_NAME}/<path:req_path>')
+def render_log_dir(req_path):
+    os.makedirs(LOG_FOLDER_NAME, exist_ok=True)
+    # Joining the base and the requested path
+    logging.info(f"req_path: {req_path}")
+    abs_path = os.path.join(req_path)
+    print(abs_path)
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        log_df = get_log_dataframe(abs_path)
+        context = {"log": log_df.to_html(classes="table-striped", index=False)}
+        return render_template('log.html', context=context)
+
+    # Show directory contents
+    files = {os.path.join(abs_path, file): file for file in os.listdir(abs_path)}
+
+    result = {
+        "files": files,
+        "parent_folder": os.path.dirname(abs_path),
+        "parent_label": abs_path
+    }
+    return render_template('log_files.html', result=result)
 
 
 if __name__ == '__main__':
